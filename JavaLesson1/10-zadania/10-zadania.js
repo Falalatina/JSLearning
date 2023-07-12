f47b66ba93e298e2cc651955681864012ebbaca5
Falalatina <karolina40.gorska17@gmail.com>
2023-07-12 11:47:46 +0200

    Update 10-zadania.js

--- JavaLesson1/10-zadania/10-zadania.js
+++ JavaLesson1/10-zadania/10-zadania.js
@@ -5,10 +5,24 @@ console.log(x);
 
 function toggle(className){
   const elementBtn = document.querySelector(className);
+  
 
   if(!elementBtn.classList.contains('toggled-btn')){
+    removeOldToggle();
     elementBtn.classList.add('toggled-btn');
   } else{
-    elementBtn.classList.remove('toggled-btn')
+    elementBtn.classList.remove('toggled-btn');
+    
+  }
+  // if(elementBtn!==jaki){
+  //   elementBtn.classList.remove('toggled-btn');
+  // }
+  // let jaki = localStorage.getItem(elementBtn);
+}
+
+function removeOldToggle(){
+  const prevBtn = document.querySelector('.toggled-btn');
+  if(prevBtn){
+    prevBtn.classList.remove('toggled-btn');
   }
 }
