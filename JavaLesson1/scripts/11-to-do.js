9e59e9574885aaaef8e10ba570576075864f0fb0
Falalatina <karolina40.gorska17@gmail.com>
2023-07-17 08:55:58 +0200

    1part loops arrays

--- JavaLesson1/scripts/11-to-do.js
+++ JavaLesson1/scripts/11-to-do.js
@@ -1,4 +1,18 @@
+
 const todoList = [];
+
+function renderTodoList(){
+  let todoListHTML = '';
+
+  for(let i = 0; i < todoList.length; i++){
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}  
 
 function addTodo(){
   const inputElement = document.querySelector('.js-input');
@@ -11,4 +25,5 @@ function addTodo(){
   //   .innerText = todoList;
 
   inputElement.value = '';
+  renderTodoList();
 }
