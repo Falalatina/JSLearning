0ada07292f6454121f079479720e325f72d6a496
Falalatina <karolina40.gorska17@gmail.com>
2023-07-20 13:17:14 +0200

    Added autoplay to game rps

--- JavaLesson1/scripts/12-rock-paper-scissors.js
+++ JavaLesson1/scripts/12-rock-paper-scissors.js
@@ -6,6 +6,24 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
+let isAutoPlaying = false;
+let intervalId;
+const AutoBtn = document.querySelector('.auto-play-btn');
+
+function autoPlay(){
+  if(!isAutoPlaying){
+      intervalId = setInterval(function(){
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    },1000);
+    isAutoPlaying = true;
+ 
+  }else{
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
+
 function playGame(playerMove){
 const computerMove = pickComputerMove();
 result = '';
