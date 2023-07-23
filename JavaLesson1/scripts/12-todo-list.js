7075faf851b37b0a8e38b7940e1e427896d54437
Falalatina <karolina40.gorska17@gmail.com>
2023-07-23 20:40:15 +0200

    todo na eventyListenery

--- JavaLesson1/scripts/12-todo-list.js
+++ JavaLesson1/scripts/12-todo-list.js
@@ -4,23 +4,36 @@ const todoList = [];
 function renderTodoList(){
   let todoListHTML = '';
 
-  todoList.forEach(todoObject, index =>{
+  todoList.forEach((todoObject, index) =>{
    // const todoObject = todoList[index];
     const {name, dueDate} = todoObject;
     const html = `
     <div>${name}</div> 
     <div>${dueDate}</div>
-    <button onclick="
-      todoList.splice(${index}, 1);
-      renderTodoList();
-    " class="delete-todo-button">Delete</button> 
+    <button class="delete-todo-button js-delete-btn">Delete</button> 
     `;
     todoListHTML += html;
   });
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-btn')
+    .forEach((deleteBtn, index) => {
+      deleteBtn.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });  
+    
 }  
+
+document.querySelector('.js-add-todo-btn')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+
 
 function addTodo(){
   const inputElement = document.querySelector('.js-input');
