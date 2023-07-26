a8e6cce4dad6146a86ab7e3dc8001e7c2b7ca3e7
Falalatina <karolina40.gorska17@gmail.com>
2023-07-26 09:10:59 +0200

    Update amazon.js

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -39,9 +39,9 @@ products.forEach((product)=>{
 
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-${product.rating * 10}.png">
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
       <div class="product-rating-count link-primary">
-        ${product.count}
+        ${product.rating.count}
       </div>
     </div>
 
