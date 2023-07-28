8e8b521e291c0fc13a90220cd1f868d79e79b106
Falalatina <karolina40.gorska17@gmail.com>
2023-07-28 09:25:11 +0200

    update

--- js-starter-me-solo/scripts/amazon.js
+++ js-starter-me-solo/scripts/amazon.js
@@ -5,23 +5,23 @@ products.forEach((product) => {
   <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        src="${product.image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
-      Black and Gray Athletic Cotton Socks - 6 Pairs
+      ${product.name}
     </div>
 
     <div class="product-rating-container">
       <img class="product-rating-stars"
-        src="images/ratings/rating-45.png">
+        src="images/ratings/rating-${product.raiting.stars/10}.png">
       <div class="product-rating-count link-primary">
-        87
+        ${product.raiting.count}
       </div>
     </div>
 
     <div class="product-price">
-      $10.90
+      $${(product.priceCents/100).toFixed(2)}
     </div>
 
     <div class="product-quantity-container">
@@ -53,5 +53,5 @@ products.forEach((product) => {
   `; 
 });
 
-document.querySelector('.products-grid')
+document.querySelector('.js-grid')
   .innerHTML = renderHTML;
